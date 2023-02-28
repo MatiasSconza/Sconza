227472ad8ccb3c65d89f9971457dbd177df7fa8f
Matías <matiassconza@gmail.com>
2023-02-27 21:55:25 -0300

    CV TICMAS

---
+++ scripts-cv.js
@@ -0,0 +1,20 @@
+const contacto= document.querySelector("#info");
+contacto.addEventListener("click", e =>{
+    document.getElementById("lista1").style.display = "block";
+}) 
+
+const idiomas= document.querySelector("#idiomas");
+idiomas.addEventListener("click", e =>{
+    document.getElementById("lista2").style.display = "block";
+}) 
+
+const aptitudes= document.querySelector("#aptitudes");
+aptitudes.addEventListener("click", e =>{
+    document.getElementById("lista3").style.display = "block";
+}) 
+
+const habilidades= document.querySelector("#habilidades");
+habilidades.addEventListener("click", e =>{
+    document.getElementById("lista4").style.display = "block";
+}) 
+
